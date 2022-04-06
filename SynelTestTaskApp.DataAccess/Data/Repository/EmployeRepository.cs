2c33046fbd9e5ab0315e79f4059adbcad901781e
Yusufboy Atajanov <Yusufboy_Atajanov@epam.com>
2022-04-06 14:36:03 +0500

    Repository pattern initialize in DataAccessProject

---
+++ SynelTestTaskApp.DataAccess/Data/Repository/EmployeRepository.cs
@@ -0,0 +1,20 @@
+﻿using SynelTestTaskApp.DataAccess.Data.Repository.IRepository;
+using SynelTestTaskApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynelTestTaskApp.DataAccess.Data.Repository
+{
+    internal class EmployeRepository:Repository<Employe>, IEmployeRepository
+    {
+        public EmployeRepository( AppDbContext dbContext)
+            : base(dbContext)
+        {
+
+        }
+
+    }
+}
---
+++ SynelTestTaskApp.DataAccess/Data/Repository/IRepository/IEmployeRepository.cs
@@ -0,0 +1,13 @@
+﻿using SynelTestTaskApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynelTestTaskApp.DataAccess.Data.Repository.IRepository
+{
+    public interface IEmployeRepository : IRepository<Employe>
+    {
+    }
+}
