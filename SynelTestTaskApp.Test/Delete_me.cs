651b2de53139694ae78c7c569b00ea4767d23406
Yusufboy Atajanov <Yusufboy_Atajanov@epam.com>
2022-04-06 13:40:11 +0500

    Xunit Test initialize

---
+++ SynelTestTaskApp.Test/Delete_me.cs
@@ -0,0 +1,12 @@
+using System;
+using Xunit;
+
+namespace SynelTestTaskApp.Test
+{
+    public class Delete_me
+    {
+        [Fact]
+        public void Test1()=>Assert.True(true);
+        
+    }
+}
